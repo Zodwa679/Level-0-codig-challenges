d905fec423afd17965fc428e12df19da060e0efb
Zodwa679 <71149511+Zodwa679@users.noreply.github.com>
2021-02-22 08:11:16 -0800

    edited the code to print 0 hours and 0 minutes when the input is 0

--- Task0.8.js
+++ Task0.8.js
@@ -3,17 +3,17 @@ function  convertNumberTime(number){
     let minutes = number % 60;
     let hours = (number - minutes )/60 ;
 
-    let hourPluralCheck = hours < 2 ? "hour " : "hours ";
+    let hourPluralCheck = hours == 1 ? "hour " : "hours ";
 
     let minuteText = minutes + ' ' + 'minutes';
     let hourText = hours + ' ' + hourPluralCheck;
 
     if(hours == 0) {
-         console.log(minuteText); 
+         console.log(hourText + minuteText); 
     }
     else {
-        console.log( minutes >= 0 ? hourText + minuteText : hourText);
+        console.log( minutes >= 0 ? hourText + minuteText: hours);
     }
 }
     
-(convertNumberTime(120));
+(convertNumberTime(60));
