1ffaee3140cb6fce1723e5065f719bdef5eb3101
Zodwa679 <71149511+Zodwa679@users.noreply.github.com>
2021-02-22 05:43:11 -0800

    Update Task0.8.js
    changed minute to minutes

--- Task0.8.js
+++ Task0.8.js
@@ -3,18 +3,17 @@ function  convertNumberTime(number){
     let minutes = number % 60;
     let hours = (number - minutes )/60 ;
 
-    let minutepluralCheck = minutes < 2 ? "munite" : "munites"; 
     let hourPluralCheck = hours < 2 ? "hour " : "hours ";
 
-    let muniteText = minutes + ' ' + minutepluralCheck;
+    let minuteText = minutes + ' ' + 'minutes';
     let hourText = hours + ' ' + hourPluralCheck;
 
     if(hours == 0) {
-         console.log(muniteText); 
+         console.log(minuteText); 
     }
     else {
-        console.log(minutes > 0 ? hourText + muniteText : hourText); 
+        console.log( minutes >= 0 ? hourText + minuteText : hourText);
     }
 }
     
-convertNumberTime(133);
+(convertNumberTime(60));
