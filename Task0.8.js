d10c08a40517f0bee633ace97d275278613627ef
Zodwa679 <71149511+Zodwa679@users.noreply.github.com>
2021-02-25 02:35:48 -0800

    fixed the spelling of minutes

--- Task0.8.js
+++ Task0.8.js
@@ -3,17 +3,17 @@ function  convertNumberTime(number){
     let minutes = number % 60;
     let hours = (number - minutes )/60 ;
 
-    let minutepluralCheck = minutes == 1 ? "munite" : "munites"; 
+    let minutepluralCheck = minutes == 1 ? "minute" : "minutes"; 
     let hourPluralCheck = hours == 1 ? "hour " : "hours ";
 
     let muniteText = minutes + ' ' + minutepluralCheck;
     let hourText = hours + ' ' + hourPluralCheck;
 
     if(hours == 0) {
-         return hourText + muniteText; 
+         return hourText + minuteText; 
     }
     else {
-        return minutes >= 0 ? hourText + muniteText : hourText; 
+        return minutes >= 0 ? hourText + minuteText : hourText; 
     }
 }
     
